33d3e18601be911c301a6b5cf325b2ff3072cd9e
fengshuan <1263215592@qq.com>
2018-09-22 19:47:35 +0800

    feat:tech-blog 项目初始化

---
+++ src/common/header/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render(){
+        return(<div>
+            this is header
+        </div>)
+    }
+}
+
+export default Header;
