0e7ff595fe7ba1818fe30400fd8dae1988bbbd7f
fengshuan <1263215592@qq.com>
2018-09-23 22:10:36 +0800

    feat:init

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component{
     render(){
         return(<div>
-            this is header
+            Header Component!
         </div>)
     }
 }
