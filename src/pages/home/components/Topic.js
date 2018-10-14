d7766a2bb378b7854a4918e558edf2b931ea2d23
fengshuan <1263215592@qq.com>
2018-10-14 11:21:48 +0800

    feat:首页专题部分构建

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { TopicWrapper, TopicItem } from '../style'
 
 class Topic extends Component{
     render(){
         return(
-            <div>there is Topic</div>
+            <TopicWrapper>
+                {
+                    this.props.list.map((item) => {
+                        return (
+                            <TopicItem key={item.get('id')}>
+                                <img 
+                                    className="topic-pic"
+                                    src={item.get('imgUrl')}
+                                    alt=""
+                                />
+                                {item.get('title')}
+                            </TopicItem>
+                        )
+                    })
+                }            
+            </TopicWrapper>
         )
     }
 }
 
-export default Topic;
+const mapStateToProps = (state) => {
+    return {
+        list: state.getIn(['home', 'topicList'])
+    }
+}
+
+export default connect(mapStateToProps, null)(Topic);
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
 export const HomeWrapper = styled.div`
-    // overflow: hidden;
     width: 960px;
     margin: 0 auto;
-    background:red;
 `;
 
 export const HomeLeft = styled.div`
@@ -22,4 +20,31 @@ export const HomeRight = styled.div`
     width: 240px;
     float: right;
     background:orange;
+`;
+
+export const TopicWrapper = styled.div`
+    overflow: hidden;
+    padding: 20px 0 10px 0;
+    margin-left: -18px;
+`;
+
+export const TopicItem = styled.div`
+    float: left;
+    height: 32px;
+    line-height: 32px;
+    padding-right: 10px;
+    margin-left: 18px;
+    margin-bottom: 10px;
+    background: #f7f7f7;
+    font-size: 14px;
+    color: #000;
+    border: 1px solid #dcdcdc;
+    border-radius: 4px;
+    .topic-pic {
+        display: block;
+        float: left;
+        width: 32px;
+        height: 32px;
+        margin-right: 10px;
+    }
 `
